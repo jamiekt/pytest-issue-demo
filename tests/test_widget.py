0ca6bd09eb1bf0263927e9ba51423e7d069f17d7
Jamie Thomson <jamie.thomson@dunnhumby.com>
2019-07-22 22:17:35 +0100

    slight mod

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,14 +3,9 @@ import mock
 from widget import Widget
 
 
-env_vars_dict = {
-        "SOME_VAR": "qwerty"
-    }
-
-
 @mock.patch.dict(
     "widget.os.environ",
-    env_vars_dict
+    {"SOME_VAR": "qwerty"}
 )
 class TestWidget(unittest.TestCase):
     def test1(self):
